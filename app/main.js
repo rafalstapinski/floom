e303b5ab65ae614a0c51643fd7226633bbf19dae
Rafal Stapinski <stapinskirafal@gmail.com>
2016-06-01 12:13:23 -0400

    some app changes

--- app/main.js
+++ app/main.js
@@ -1,4 +1,7 @@
+module.paths.push('/usr/local/lib/node_modules');
+
 const electron = require("electron");
+const player = require("player");
 
 const {app} = electron;
 const {BrowserWindow} = electron;
@@ -9,7 +12,7 @@ function createWindow() {
 
   win = new BrowserWindow({width: 800, height: 600});
   win.loadURL(`file://${__dirname}/index.html`)
-  //win.webContents.openDevTools();
+  win.webContents.openDevTools();
 
   win.on("closed", () => {
     win = null;
